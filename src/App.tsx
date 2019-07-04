7327891271572447c1b7425d782d73498c14dcb7
Justin Wang <junting.wang@thoughtworks.com>
2019-07-04 15:09:02 +0800

    test tslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import React, {Component} from 'react';
-import {Platform, Text, View} from 'react-native';
-import styles from './App.scss';
+import React, { Component } from 'react'
+import { Platform, Text, View } from 'react-native'
+import styles from './App.scss'
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
     'Double tap R on your keyboard to reload,\n' +
     'Shake or press menu button for dev menu',
-});
+})
 
-type Props = {};
+type Props = {}
 export default class App extends Component<Props> {
   render() {
     return (
@@ -18,6 +18,6 @@ export default class App extends Component<Props> {
         <Text className={styles.instructions}>To get started, edit App.js</Text>
         <Text className={styles.instructions}>{instructions}</Text>
       </View>
-    );
+    )
   }
 }
